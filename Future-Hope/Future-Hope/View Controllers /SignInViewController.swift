618ea5901e725bd966d84d80ea303f9768e5ed0c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-19 16:52:12 -0700

    signInView outlets

--- Future-Hope/Future-Hope/View Controllers /SignInViewController.swift
+++ Future-Hope/Future-Hope/View Controllers /SignInViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+
+// Firebase
 import Firebase
 import FirebaseAuth
 import GoogleSignIn
 
+// Material
+import MaterialComponents.MaterialTextFields
 
 class SignInViewController: UIViewController{
-
+	
+	
+	@IBOutlet var emailTextField: MDCTextField!
+	@IBOutlet var passwordTextField: MDCTextField!
+	
+	
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 		GIDSignIn.sharedInstance()?.presentingViewController = self
 		
     }
 
+	@IBAction func logInButtonPressed(_ sender: UIButton) {
+		print("logged in with email/password")
+	
+	}
 	
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		
