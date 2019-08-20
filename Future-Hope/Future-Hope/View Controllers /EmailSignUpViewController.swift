6305d2dd59ba0c43167c3c573787d08084865a0b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-20 09:47:38 -0700

    email sign in View

---
+++ Future-Hope/Future-Hope/View Controllers /EmailSignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmailSignUpViewController.swift
+//  Future-Hope
+//
+//  Created by Hector Steven on 8/20/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class EmailSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
