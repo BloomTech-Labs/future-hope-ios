83435a6556e2f58d9aebfb76b4f0f2e4117ca5a9
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-26 23:07:49 -0700

    slieOutMenu

---
+++ Future-Hope/Future-Hope/SlideOutMenu/SlideOutMenuContainerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SlideOutMenuContainerViewController.swift
+//  Future-Hope
+//
+//  Created by Hector Steven on 8/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutMenuContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
