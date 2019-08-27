7ad56b5abda863e1aa24eb246f32b8339999cf98
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-26 23:56:08 -0700

    SlideOutMenuContainer

--- Future-Hope/Future-Hope/SlideOutMenu/SlideOutMenuContainerViewController.swift
+++ Future-Hope/Future-Hope/SlideOutMenu/SlideOutMenuContainerViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-protocol  SlideOutMenuContainerViewControllerDelegate{
+protocol SlideOutMenuContainerViewControllerDelegate{
 	func didSelectIndex(_ indexPath: IndexPath)
 }
 
 
 class SlideOutMenuContainerViewController: UIViewController {
 
+	
+	@IBOutlet var tableView: UITableView!
+	
+	var delegate: SlideOutMenuContainerViewControllerDelegate?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
+		tableView.delegate = self
+		tableView.dataSource = self
     }
-    
 
 
+}
+
+
+extension SlideOutMenuContainerViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "SlideOutCell", for: indexPath)
+		return cell
+	}
+	
+	
+	
+}
+
+
+extension SlideOutMenuContainerViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		delegate?.didSelectIndex(indexPath)
+	}
 }
