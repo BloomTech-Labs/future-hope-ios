620bca3252f23739887510e9cebe2b04dd1e6c1b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-26 23:20:26 -0700

    slide panel

---
+++ Future-Hope/Future-Hope/SlideOutMenu/SidePanelViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SidePanelViewController.swift
+//  Future-Hope
+//
+//  Created by Hector Steven on 8/26/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class SidePanelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
