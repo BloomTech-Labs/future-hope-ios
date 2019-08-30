d47bb6e201230aa2b2b3fe7a3f7f44dc961b78e6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-29 19:05:59 -0700

    dash board

--- Future-Hope/Future-Hope/MainViewController.swift
+++ Future-Hope/Future-Hope/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 class MainViewController: UIViewController {
-
+	private let settings = ["About Me", "Account Status", "city", "country","email", "phone number", "State/Province"]
 	var currentSignedInUser: CurrentUser?
 	let futureHopeController = ApplicationController()
 	
@@ -18,33 +18,34 @@ class MainViewController: UIViewController {
 	@IBOutlet var namelabel: UILabel!
 	@IBOutlet var emailLabel: UILabel!
 	@IBOutlet var userTypeLabel: UILabel!
+	@IBOutlet var tableView: UITableView!
+	@IBOutlet var aboutMeTextView: UITextView!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		tableView.delegate = self
+		tableView.dataSource = self
+	}
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		
-		
+		fetchCurrentAuthUser()
+	}
+	
+	private func fetchCurrentAuthUser() {
 		guard let user = futureHopeController.fetchCurrentAuthenticatedUser() else { return }
 		FireStoreController().fetchUser(uuid: user.uid) { user, error in
 			if let error = error {
 				NSLog("Error: \(error)")
 				return
 			}
-			
 			self.currentSignedInUser = user
-			
 			self.setupViews()
-			
 		}
-		
 	}
 	
-	override func viewDidLoad() {
-        super.viewDidLoad()
-		
-		
-		
-    }
-
+	
 	private func setupViews() {
 		guard let currentSignedInUser = currentSignedInUser else { return }
 		
@@ -53,48 +54,64 @@ class MainViewController: UIViewController {
 		namelabel?.text = currentSignedInUser.fullName
 		emailLabel?.text = currentSignedInUser.email
 		userTypeLabel?.text = currentSignedInUser.userType == .mentor ? "mentor" : "teacher"
+		aboutMeTextView?.text = currentSignedInUser.aboutMe
 		
-		
-		
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@IBAction func SldieOutMenuButtonPressed(_ sender: Any) {
-		print("Slide out!")
-
+		futureHopeController.fetchUserImage(with: currentSignedInUser.photoUrl) { data, error in
+			if let error = error {
+				NSLog("Error fetching user image: \(error)")
+				return
+			}
+			
+			guard let data = data else { return }
+			
+			DispatchQueue.main.async {
+				self.imageView.image = UIImage(data: data)				
+			}
+		}
 	}
 
 }
 
 
-//protocol ToggleSlideOutMenuDelegate {
-//	func toggleSlideOutMenu()
-//	func collapseSidePanels()
-//}
-//
-//class MainViewController: UIViewController {
-//
-//	var delegate: ToggleSlideOutMenuDelegate?
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
-//
-//
-//	@IBAction func SldieOutMenuButtonPressed(_ sender: Any) {
-//		print("Slide out!")
-//
-//	}
-//
-//}
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return settings.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "dashBoardCell", for: indexPath)
+		cell.textLabel?.text = settings[indexPath.row]
+		cell.detailTextLabel?.text = getUserData(with: indexPath.row)
+		return cell
+	}
+	
+	
+	
+	private func getUserData(with row: Int) -> String? {
+		guard let currentSignedInUser = currentSignedInUser else { return nil }
+		if row == 0 {
+			return "edit"
+		} else if row == 1 {
+			return currentSignedInUser.awaitingApproval == true ? "awaiting approval" : "approved"
+		} else if row == 2 {
+			return currentSignedInUser.city
+		} else if row == 3 {
+			return currentSignedInUser.country
+		}else if row == 4 {
+			return currentSignedInUser.email
+		}else if row == 5 {
+			return currentSignedInUser.phoneNumber
+		}
+		
+		
+		
+		
+		return nil
+	
+	}
+	
+	
+	
+	
+}
+
