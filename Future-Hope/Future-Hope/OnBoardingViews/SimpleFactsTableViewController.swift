4d1bd8b3d1d072522035cb068fc9a6adb3832cd6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-30 21:40:54 -0700

    facts tableView

---
+++ Future-Hope/Future-Hope/OnBoardingViews/SimpleFactsTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SimpleFactsTableViewController.swift
+//  Future-Hope
+//
+//  Created by Hector Steven on 8/30/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class SimpleFactsTableViewController: UITableViewController {
+	let faqStrings = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+					  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+					  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+					  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."]
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Exit", style: .done, target: self, action: #selector(exitButton))
+    }
+	
+	@objc func exitButton() {
+		navigationController?.dismiss(animated: true)
+	}
+	
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return faqStrings.count
+    }
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "FaQCell", for: indexPath)
+		cell.textLabel?.text = "FAQ\(indexPath.row + 1)"
+		cell.detailTextLabel?.text = faqStrings[indexPath.row]
+		return cell
+	}
+
+}
