1b78d397c5e290704d231ef7715eddb41cc56a7b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-03 23:27:14 -0700

    feature/fixed

--- Future-Hope/Future-Hope/FutureHopeschoolViews/TabBarViewController.swift
+++ Future-Hope/Future-Hope/FutureHopeschoolViews/TabBarViewController.swift
@@ -23,9 +23,7 @@ class TabBarViewController: UITabBarController {
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		print("uid: \(uid)")
-		
+		getCurrentUser()
 		for childVC in children {
 			if let vc = childVC as? FutureHopSchoolControllerProtocol {
 				vc.futureHopSchoolController = futureHopSchoolController
@@ -34,20 +32,21 @@ class TabBarViewController: UITabBarController {
     }
 	
 	
-	private func getCurrentUser(_ uid: String) {
-//		guard let user = futureHopSchoolController.fetchCurrentFireAuthenticatedUser() else { return }
-		
-		//check firebase "user" with uid
+	private func getCurrentUser() {
+		guard let uid = uid else { return }
 		FireStoreController.db.collection(FireStoreController.users).document(uid).getDocument { document, error in
 			if let error = error {
 				NSLog("Error fetching user from firestore: \(error)")
 				return
 			}
-				
+			
+			print("Found!!!!!")
 			if let doc = document, doc.exists, let data = doc.data() {
 				if let currentUser = CurrentUser(dictionary: data as [String: Any]) {
 					self.futureHopSchoolController.setCurrentlyLogedInUser(with: currentUser)
-//					print("found account")
+					print("found account")
+				}else {
+					print("Error getting data from fire store!")
 				}
 			}else {
 //				self.createUser(user)
