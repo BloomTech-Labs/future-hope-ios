de6de95865e7bf0aecec637726fc8b58d0ef9dfa
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-04 10:10:02 -0700

    editprofileview

---
+++ Future-Hope/Future-Hope/FutureHopeschoolViews/EditProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditProfileViewController.swift
+//  Future-Hope
+//
+//  Created by Hector Steven on 9/4/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+import MaterialComponents.MaterialTextFields
+
+class EditProfileViewController: UIViewController {
+	@IBOutlet var editLabel: UILabel!
+	@IBOutlet var newEditTextField: MDCTextField!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+    }
+
+	
+	@IBAction func exitButtonPressed(_ sender: Any) {
+		dismiss(animated: true)
+	}
+	
+	
+	
+}
