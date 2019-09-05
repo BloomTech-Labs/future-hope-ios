bc3849ce2dbc9c12c30ea679c7e498073fc653f8
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-05 12:04:15 -0700

    MySchedual Cell

--- Future-Hope/Future-Hope/OnBoardingViews/MySchedualViewController.swift
+++ Future-Hope/Future-Hope/OnBoardingViews/MySchedualViewController.swift
@@ -9,13 +9,38 @@
 import UIKit
 
 class MySchedualViewController: UIViewController {
-
-    override func viewDidLoad() {
+	@IBOutlet var numberOfMettingsLabel: UILabel!
+	@IBOutlet var tableView: UITableView!
+	
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
     }
 	
-	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		
+	}
 	
 	
 
+}
+
+extension MySchedualViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "MySchedualCell", for: indexPath)
+		
+		return cell
+	}
+	
+	
+	
+	
+	
+	
 }
