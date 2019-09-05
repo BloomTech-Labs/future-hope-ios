4f93ddf25badd7a5f95cde6b4266cce90026981c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-05 15:16:47 -0700

    myschedual tableview cell

---
+++ Future-Hope/Future-Hope/OnBoardingViews/MyScdualTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyScdualTableViewCell.swift
+//  Future-Hope
+//
+//  Created by Hector Steven on 9/5/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class MyScdualTableViewCell: UITableViewCell {
+
+  
+
+}
