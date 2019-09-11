5843428514d91be0d817743f03b216a8cc00baed
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-11 10:48:48 -0700

    exploere tableview and cell

---
+++ Future-Hope/Future-Hope/FutureHopeschoolViews/ExploreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreTableViewCell.swift
+//  Future-Hope
+//
+//  Created by SL Hector Villasano on 9/11/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ExploreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
