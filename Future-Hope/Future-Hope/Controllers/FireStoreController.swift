4f9ac4d243c4c797b26045d84a0f0a6c6cc2cdce
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-12 11:58:37 -0700

    fetchMyMeetings

--- Future-Hope/Future-Hope/Controllers/FireStoreController.swift
+++ Future-Hope/Future-Hope/Controllers/FireStoreController.swift
@@ -37,67 +37,67 @@ struct FireStoreController {
 		}
 	}
     
-    func fetchAllUsers(completion: @escaping ([CurrentUser]?, Error?) -> ()){
-        usersColectionRef.getDocuments { documentsSnapShot, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let snap = documentsSnapShot else { return }
-            
-            var users: [CurrentUser] = []
-            for document in snap.documents {
-                let dict = document.data() as [String: Any]
-                if let user = CurrentUser(dictionary: dict) {
-                    users.append(user)
-                }
-            }
-            
-            completion(users, nil)
-        }
-    }
+//    func fetchAllUsers(completion: @escaping ([CurrentUser]?, Error?) -> ()){
+//        usersColectionRef.getDocuments { documentsSnapShot, error in
+//            if let error = error {
+//                completion(nil, error)
+//                return
+//            }
+//
+//            guard let snap = documentsSnapShot else { return }
+//
+//            var users: [CurrentUser] = []
+//            for document in snap.documents {
+//                let dict = document.data() as [String: Any]
+//                if let user = CurrentUser(dictionary: dict) {
+//                    users.append(user)
+//                }
+//            }
+//
+//            completion(users, nil)
+//        }
+//    }
     
-    func fetchMeetings(completion: @escaping ([Meeting]?, Error?) -> ()) {
-       meetingsCollectionRef.getDocuments { documentsSnapShot, error in
-            if let error =  error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let snap = documentsSnapShot else { return }
-            
-            var meetings: [Meeting] = []
-            for document in snap.documents {
-                let dict = document.data() as [String: Any]
-                
-                if let meeting = Meeting(dictionary: dict) {
-                    meetings.append(meeting)
-                }
-            }
-            
-            completion(meetings, nil)
-        }
-    }
-    
-    func fetchMeeting(with uid: String, completion: @escaping (Meeting?, Error?) -> ()) {
-       meetingsCollectionRef.document(uid).getDocument { documentSnapShot, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let doc = documentSnapShot,
-                let data = doc.data() else { return }
-            
-            let dict = data as [String: Any]
-            if let meeting = Meeting(dictionary: dict) {
-                completion(meeting, nil)
-            }
-        }
-    }
-    
-    
+//    func fetchMeetings(completion: @escaping ([Meeting]?, Error?) -> ()) {
+//       meetingsCollectionRef.getDocuments { documentsSnapShot, error in
+//            if let error =  error {
+//                completion(nil, error)
+//                return
+//            }
+//
+//            guard let snap = documentsSnapShot else { return }
+//
+//            var meetings: [Meeting] = []
+//            for document in snap.documents {
+//                let dict = document.data() as [String: Any]
+//
+//                if let meeting = Meeting(dictionary: dict) {
+//                    meetings.append(meeting)
+//                }
+//            }
+//
+//            completion(meetings, nil)
+//        }
+//    }
+//
+//    func fetchMeeting(with uid: String, completion: @escaping (Meeting?, Error?) -> ()) {
+//       meetingsCollectionRef.document(uid).getDocument { documentSnapShot, error in
+//            if let error = error {
+//                completion(nil, error)
+//                return
+//            }
+//
+//            guard let doc = documentSnapShot,
+//                let data = doc.data() else { return }
+//
+//            let dict = data as [String: Any]
+//            if let meeting = Meeting(dictionary: dict) {
+//                completion(meeting, nil)
+//            }
+//        }
+//    }
+//
+    // MARK:
     func fetchAllTeachers(completion: @escaping ([CurrentUser]?, Error?) -> ()) {
         let whereField =  usersColectionRef.whereField("userType", isEqualTo: "teacher")
         whereField.getDocuments { documentsSnapShot, error in
@@ -118,11 +118,34 @@ struct FireStoreController {
             
             completion(users, nil)
         }
-        
-        
-        
     }
     
+    
+    func fetchMyMeetings(with uid: String, completion: @escaping ([Meeting]?, Error?) -> ()) {
+        
+        
+        let whereField = meetingsCollectionRef.whereField("participantUIDs", arrayContains: uid)
+        
+        whereField.getDocuments { meetingsSnapShot, error in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let meetings = meetingsSnapShot else { return }
+            
+            var myMeetings: [Meeting] = []
+            for meeting in meetings.documents {
+                let dictioanry = meeting.data() as [String: Any]
+                
+                if let meeting = Meeting(dictionary: dictioanry) {
+                    myMeetings.append(meeting)
+                }
+            }
+            
+            completion(myMeetings, nil)
+        }
+    }
     
     
     
